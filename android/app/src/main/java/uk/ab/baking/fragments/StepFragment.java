18c1d62cf9b40ebb09ff5bb11df4a066fe4a7ec3
aboyce <adam@aboyce.uk>
2019-02-16 11:42:26 +0000

    Added multiple views for the landscape video on mobile, separated out layouts for reuse, added layout based configuration for the video size, added checks to display/hide the description when requried.

--- android/app/src/main/java/uk/ab/baking/fragments/StepFragment.java
+++ android/app/src/main/java/uk/ab/baking/fragments/StepFragment.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,6 +19,7 @@ import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
@@ -32,16 +35,23 @@ import uk.ab.baking.viewmodels.RecipeViewModel;
 
 public class StepFragment extends Fragment implements Player.EventListener {
 
+    private static final boolean START_VIDEO_WHEN_READY = false;
+
     private RecipeViewModel viewModel;
 
     private SimpleExoPlayer videoPlayer;
 
+    @BindView(R.id.fragment_step_cl_video)
+    ConstraintLayout videoPlayerContainer;
+
     @BindView(R.id.fragment_step_sepv_player)
     PlayerView videoPlayerView;
 
+    @Nullable
     @BindView(R.id.fragment_step_instructions_tv_short_description)
     TextView stepShortDescription;
 
+    @Nullable
     @BindView(R.id.fragment_step_instructions_tv_description)
     TextView stepDescription;
 
@@ -60,10 +70,7 @@ public class StepFragment extends Fragment implements Player.EventListener {
         View rootView = inflater.inflate(R.layout.fragment_step, container, false);
         ButterKnife.bind(this, rootView);
         setupViewModelEvents();
-
-        // TODO: Remove the hardcoded Url.
-        String videoUrl = "https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4";
-        initialiseVideoPlayer(Uri.parse(videoUrl));
+        loadVideoPlayer();
         return rootView;
     }
 
@@ -78,8 +85,18 @@ public class StepFragment extends Fragment implements Player.EventListener {
         viewModel.getClickedOnStep().observe(this, step -> {
             Timber.d("ViewModel step has been updated.");
             if (step != null) {
-                stepShortDescription.setText(step.getShortDescription());
-                stepDescription.setText(step.getDescription());
+                if (stepDescription != null) {
+                    stepDescription.setText(step.getDescription());
+                }
+                if (stepShortDescription != null) {
+                    stepShortDescription.setText(step.getShortDescription());
+                }
+                if (step.getVideoUrl() != null) {
+                    loadContentVideoPlayer(step.getVideoUrl());
+                    Timber.v("Video to be loaded '" + step.getVideoUrl() + "'.");
+                } else {
+                    Timber.w("There is no video provided for step '" + step.getShortDescription() + "'.");
+                }
             } else {
                 Timber.w("ViewModel step was null.");
             }
@@ -96,19 +113,38 @@ public class StepFragment extends Fragment implements Player.EventListener {
         }
     }
 
-    private void initialiseVideoPlayer(Uri mediaUri) {
+    private void loadVideoPlayer() {
         if (videoPlayer != null) {
-            Timber.d("The video player is already initialised.");
+            Timber.d("The video player is already loaded.");
+            return;
+        }
+        videoPlayer = ExoPlayerFactory.newSimpleInstance(getContext());
+        videoPlayerView.setPlayer(videoPlayer);
+        // We want to keep the aspect ratio to ensure that the video looks as it should.
+        videoPlayerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIT);
+        videoPlayer.setPlayWhenReady(START_VIDEO_WHEN_READY);
+    }
+
+    private void loadContentVideoPlayer(String videoUriRaw) {
+        if (videoPlayer == null) {
+            Timber.w("The video player is null, make sure you call loadVideoPlayer() first.");
             return;
         }
 
-        videoPlayer = ExoPlayerFactory.newSimpleInstance(getContext());
-        videoPlayerView.setPlayer(videoPlayer);
+        if (videoUriRaw == null || videoUriRaw.equals("")) {
+            // If there is nothing to display, hide the player.
+            videoPlayerContainer.setVisibility(View.GONE);
+            Timber.w("The raw video uri is null or empty, hiding the video player.");
+            return;
+        }
+
+        Uri videoUri = Uri.parse(videoUriRaw);
+        // If there is something to display, show the player and load the content.
+        videoPlayerContainer.setVisibility(View.VISIBLE);
         String userAgent = Util.getUserAgent(getContext(), StepFragment.class.getSimpleName());
         DefaultDataSourceFactory sourceFactory = new DefaultDataSourceFactory(getContext(), userAgent);
-        MediaSource mediaSource = new ExtractorMediaSource.Factory(sourceFactory).createMediaSource(mediaUri);
+        MediaSource mediaSource = new ExtractorMediaSource.Factory(sourceFactory).createMediaSource(videoUri);
         videoPlayer.prepare(mediaSource);
-        videoPlayer.setPlayWhenReady(true);
     }
 
     private void releaseVideoPlayer() {
